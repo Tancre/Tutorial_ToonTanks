b36f494294163d3b49a54289e8f4001a60ef8b86
Tancre <outisisout@gmail.com>
2021-05-29 12:52:26 +0200

    Creating child C++ class

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -26,15 +26,4 @@ private:
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 };
